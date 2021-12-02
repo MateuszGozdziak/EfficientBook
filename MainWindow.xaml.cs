c362d054ba39f5bda69a94c061c82bb6653927af
Konrad Dzięcioł <konradd990212@gmail.com>
2021-12-02 20:34:56 +0100

    test change 2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,6 +24,8 @@ namespace WpfApp1
         Page1 page1 = new Page1();
         Page2 page2 = new Page2();
 
+        //
+
         public MainWindow()
         {
             InitializeComponent();
