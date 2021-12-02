5e13bcbe4d0510673974b12a18d03ade03fcca99
konra <konra@DESKTOP-FJTGRSS>
2021-12-02 19:57:55 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Page1 page1 = new Page1();
+        Page2 page2 = new Page2();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Main.NavigationService.Navigate(page1);
+            //Main.NavigationService.Navigate(new Uri("Page1.xaml", UriKind.Relative));
+            //Main.Navigate(page1);
+
+
+            
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Produkty(object sender, RoutedEventArgs e)
+        {
+            //Work_Space.Fill = new SolidColorBrush(Color.FromRgb(0, 111, 111));
+            //win2.Show();
+            //Page.NavigationService.Navigate(page1);
+            //if(this.GetType() == typeof(Page1))
+            //{
+            //}
+            //Page1 page1 = new Page1();
+
+            Main.Navigate(page1);
+            
+            //Main.NavigationService.Navigate(new Page1());
+            //Main.Content = new Page1();
+            //NavigationService.Navigate(new Window1());
+            //FrameWithinGrid.Navigate(new System.Uri("Page1.xaml",
+            //UriKind.RelativeOrAbsolute));
+            //frame
+        }
+        private void Button_Klienci(object sender, RoutedEventArgs e)
+        {
+            //Main.Navigate(page2);
+            Main.NavigationService.Navigate(page2);
+        }
+
+    }
+}
---
+++ Page1.xaml.cs
@@ -0,0 +1,112 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+
+            string connStr = "server=localhost;user=root;database=projektprogramowaniezaawansowane;port=3306;password=";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                //List<string>[] list = new List<string>[3];
+                //list[0] = new List<string>();
+                //list[1] = new List<string>();
+                //list[2] = new List<string>();
+
+
+
+
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = "SELECT * FROM produkty";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+
+                List<string> ListBox_list_of_ID = new List<string>(); 
+                List<string> ListBox_list_of_Nazwa = new List<string>();
+                List<string> ListBox_list_of_Ilosc_dostepnych = new List<string>();
+                List<string> ListBox_list_of_Cenabrutto = new List<string>();
+                List<string> ListBox_list_of_Cenanetto = new List<string>();
+                List<string> ListBox_list_of_Kod = new List<string>();
+                List<string> ListBox_list_of_Marka = new List<string>(); 
+                List<string> ListBox_list_of_VAT = new List<string>();
+                //List<string> IDListBox_list = new List<string>(5);
+
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+
+                        ListBox_list_of_ID.Add(reader.GetInt32(0).ToString());
+                        ListBox_list_of_Nazwa.Add(reader.GetString(1).ToString());
+                        ListBox_list_of_Ilosc_dostepnych.Add(reader.GetInt32(2).ToString());
+                        ListBox_list_of_Cenabrutto.Add(reader.GetInt32(3).ToString());
+                        ListBox_list_of_Cenanetto.Add(reader.GetInt32(4).ToString());
+                        ListBox_list_of_Kod.Add(reader.GetString(5).ToString());
+                        ListBox_list_of_Marka.Add(reader.GetString(6).ToString());
+                        ListBox_list_of_VAT.Add(reader.GetInt32(7).ToString());
+
+                        //int a1 = reader.GetInt32(0);
+                        //MessageBox.Show(reader.GetInt32(0).ToString());
+                        //MessageBox.Show(reader.GetString(1).ToString());
+                        //MessageBox.Show(reader.GetInt32(2).ToString());
+                        //MessageBox.Show(reader.GetInt32(3).ToString());
+                        //MessageBox.Show(reader.GetInt32(4).ToString());
+                        //MessageBox.Show(reader.GetString(5).ToString());
+                        //MessageBox.Show(reader.GetString(6).ToString());
+                        //MessageBox.Show(reader.GetInt32(7).ToString());
+                        //ListBox_list_of_lists[0].Add(reader.GetInt32(0).ToString());
+
+                    }
+                    ListBox_ID.ItemsSource = ListBox_list_of_ID;
+                    ListBox_Cenabrutto.ItemsSource = ListBox_list_of_Cenabrutto;
+                    ListBox_Cenanetto.ItemsSource = ListBox_list_of_Cenanetto;
+                    ListBox_Ilosc_dostepnych.ItemsSource = ListBox_list_of_Ilosc_dostepnych;
+                    ListBox_Kod.ItemsSource = ListBox_list_of_Kod;
+                    ListBox_VAT.ItemsSource = ListBox_list_of_VAT;
+                    ListBox_Nazwa.ItemsSource = ListBox_list_of_Nazwa;
+                    ListBox_Marka.ItemsSource = ListBox_list_of_Marka;
+
+
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                MessageBox.Show(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+        }
+    }
+}
